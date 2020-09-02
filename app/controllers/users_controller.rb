22e3a0e0e07bdcff906dc4b8260c0a6417c3523e
EC2 Default User <ec2-user@ip-172-31-40-41.ap-northeast-1.compute.internal>
2020-09-02 03:46:29 +0000

    Finish user-tasks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: %i(show edit update destroy)
+  before_action :logged_in_user, only: %i(index show edit update destroy)
+  before_action :admin_user, only: %i(index destroy) 
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_or_correct, only: %i(show)
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end
+
   def show
-    @user = User.find(params[:id])
   end
 
   def new
-    @user = User.new
-  end
+    if logged_in? && !current_user.admin?
+      flash[:info] = 'すでにログインしています。'
+      redirect_to current_user
+    end 
+    @user =User.new
+  end 
   
   def create
     @user = User.new(user_params)
@@ -17,6 +29,24 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] ='ユーザー情報を更新しました。'
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
   end
   
   private
